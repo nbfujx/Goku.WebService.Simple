969ab28de180934e29815a33d2ed869d205df0c5
nbfujx <nbfujx@qq.com>
2017-11-07 09:28:15 +0800

    20171020 新增shiro文件

---
+++ src/main/java/com/goku/webapi/config/Shiro/MySessionListener.java
@@ -0,0 +1,24 @@
+package com.goku.webapi.config.Shiro;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+
+/**
+ * Created by nbfujx on 2017/11/7.
+ */
+public class MySessionListener implements SessionListener {
+    @Override
+    public void onStart(Session session) {
+
+    }
+
+    @Override
+    public void onStop(Session session) {
+
+    }
+
+    @Override
+    public void onExpiration(Session session) {
+
+    }
+}
---
+++ src/main/java/com/goku/webapi/config/Shiro/ShiroConfig.java
@@ -0,0 +1,11 @@
+package com.goku.webapi.config.Shiro;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by nbfujx on 2017/11/2.
+ */
+@Configuration
+public class ShiroConfig {
+
+}
