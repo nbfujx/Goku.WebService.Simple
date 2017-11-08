4f466e4358a43d468aadce82e92552c54695a12f
nbfujx <nbfujx@qq.com>
2017-11-08 08:32:10 +0800

    20171108  新增returnmsg

--- src/main/java/com/goku/webapi/util/message/returnMsg.java
+++ src/main/java/com/goku/webapi/util/message/returnMsg.java
@@ -1,8 +1,31 @@
 package com.goku.webapi.util.message;
 
+import com.goku.webapi.util.enums.returnCode;
+
 /**
  * Created by nbfujx on 2017-11-07.
  */
 public class returnMsg {
+
+    private returnCode Code;
+    private Object Data;
+
+
+    public returnCode getCode() {
+        return Code;
+    }
+
+    public void setCode(returnCode code) {
+        Code = code;
+    }
+
+    public Object getData() {
+        return Data;
+    }
+
+    public void setData(Object data) {
+        Data = data;
+    }
+
 
 }
